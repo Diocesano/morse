407255f8e94a1f016f9d48c1b5f72506230a2452
Diogenes Souza <diogenes@diogenesf.com>
2017-08-10 02:50:25 -0300

    Inclusão da licença de uso

--- morse/morse.ino
+++ morse/morse.ino
@@ -9,6 +9,8 @@
 
   #Orientador: Prof. Diógenes Souza - diogenessouza@diocesanocaruaru.g12.br
 
+  #Repositório do projeto no GitHub: https://github.com/Diocesano/morse
+
 
   #Descrição:
   Este projeto funciona a partir do aplicativo android
@@ -51,8 +53,8 @@
          | [ ]A1    -| O |-               4[ ] |
          | [ ]A2     +---+           INT1/3[ ]~|
          | [ ]A3                     INT0/2[ ] |
-         | [D]A4/SDA                   TX>1[ ] |
-         | [E]A5/SCL                   RX<0[ ] |
+         | [D]A4/SDA                   TX>1[F] |
+         | [E]A5/SCL                   RX<0[G] |
          |                                     |
          |  UNO_R3                 ____________/
           \_______________________/
@@ -61,7 +63,42 @@
   C: pino positivo do led
   D: pino SDA do display LCD
   E: pino SCL do display LCD
+  F: pino RXD do módulo HC-06
+  G: pino TXD do módulo HC-06
 
+
+  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    LICENÇA - Copyright 2017 Maiara, Alisson, Caio, Guilherme e Diógenes
+  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   
+
+    Este programa é um software livre; você pode redistribuí-lo e/ou 
+    modificá-lo dentro dos termos da Licença Pública Geral GNU como 
+    publicada pela Fundação do Software Livre (FSF); na versão 3 da 
+    Licença, ou (na sua opinião) qualquer versão.
+
+    Este programa é distribuído na esperança de que possa ser útil, 
+    mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+    a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+    Licença Pública Geral GNU para maiores detalhes.
+
+    Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+    com este programa. Se não, veja <http://www.gnu.org/licenses/>.
+  
+  ------------------------------------------------------------------
 */
 
 
